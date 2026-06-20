077e879f9a53d49bd8a014a6b9af362ec555a3e6
Anton <Antonioum80@gmail.com>
2026-06-20 10:13:10 +0300

    Add Day31 Docker lab

---
+++ linux/day31/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+    return conn
+
+@app.route("/")
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM test;")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(rows)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
